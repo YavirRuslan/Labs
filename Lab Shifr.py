f8dfd88d8a220e7758abc1f93cc8317d76d4fd0d
Yavir Ruslan <93978770+YavirRuslan@users.noreply.github.com>
2021-11-09 12:27:24 +0200

    Add files via upload

---
+++ Lab Shifr.py
@@ -0,0 +1,12 @@
+text = input()
+key = 1
+alf = "абвгґдеєжзиіїйклмнопрстуфхцчшщьюяабвгґдеєжзиіїйклмнопрстуфхцчшщьюяАБВГҐДЕЄЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩЬЮЯАБВГҐДЕЄЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩЬЮЯabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ12345678901234567890"
+symb ="  {{}}[[]](())--==++//**"
+result = ""
+for i in range(len(text)):
+    if str(text[i]) in alf:
+        t = alf.find(str(text[i]))
+        result += alf[t + key]
+    elif str(text[i]) in symb:
+        result += symb[symb.find(str(text[i]))]
+print(result)
